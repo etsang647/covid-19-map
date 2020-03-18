0fec45dfff077e1d9327fb5468f01cd19dc3ea7d
Eric Tsang <etsang647@gmail.com>
2020-03-18 18:26:45 -0400

    output_data now generates json files for confirmed, deaths, and recovered

--- server/output_data.py
+++ server/output_data.py
@@ -1,13 +1,121 @@
 from urllib.request import urlopen
+from contextlib import closing
 from io import StringIO
-import csv
+import csv, json
 
-url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+CONFIRMED_CSV_INPUT = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+DEATHS_CSV_INPUT = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
+RECOVERED_CSV_INPUT = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
 
-data = urlopen(url).read().decode('ascii', 'ignore')
-datafile = StringIO(data)
-cr = csv.reader(datafile)
+CONFIRMED_JSON_OUTPUT = './data/confirmed.json'
+DEATHS_JSON_OUTPUT = './data/deaths.json'
+RECOVERED_JSON_OUTPUT = './data/recovered.json'
 
-for row in cr:
-  if (row[1] == "US"):
-    print(row)
+NUM_ROWS = None
+LAST_US_ROW = None
+
+# evaluate NUM_ROWS and LAST_US_ROW
+with closing(urlopen(CONFIRMED_CSV_INPUT)) as csvfile:
+  data = csvfile.read().decode('ascii', 'ignore')
+  datafile = StringIO(data)
+  dict_reader = csv.DictReader(datafile)
+  total_rows = 0
+  last_us_row = 0
+
+  for row in dict_reader:
+    if row['Country/Region'] == 'US':
+      last_us_row = total_rows
+    total_rows += 1
+
+  NUM_ROWS = total_rows
+  LAST_US_ROW = last_us_row
+
+# generates confirmed.json
+#
+# reads data from CSV file located at the URL CONFIRMED_CSV_INPUT
+# and outputs the number of confirmed cases by date for all US states
+# as a list of JSON objects, stored in the file at CONFIRMED_JSON_OUTPUT
+with closing(urlopen(CONFIRMED_CSV_INPUT)) as csvfile:
+  with open(CONFIRMED_JSON_OUTPUT, 'w') as jsonfile:
+    data = csvfile.read().decode('ascii', 'ignore')
+    datafile = StringIO(data)
+    dict_reader = csv.DictReader(datafile)
+    line_count = 0
+
+    for row in dict_reader:
+      if line_count == 0:
+        jsonfile.write('[\n')
+
+      if row['Country/Region'] == 'US':
+        json.dump(row, jsonfile)
+
+        # append commas where necessary
+        if line_count < LAST_US_ROW - 1:
+          jsonfile.write(',')
+
+        jsonfile.write('\n')
+
+      if line_count == NUM_ROWS - 1:
+        jsonfile.write(']\n')
+
+      line_count += 1
+
+# generates deaths.json
+#
+# reads data from CSV file located at the URL CONFIRMED_CSV_INPUT
+# and outputs the number of confirmed cases by date for all US states
+# as a list of JSON objects, stored in the file at CONFIRMED_JSON_OUTPUT
+with closing(urlopen(DEATHS_CSV_INPUT)) as csvfile:
+  with open(DEATHS_JSON_OUTPUT, 'w') as jsonfile:
+    data = csvfile.read().decode('ascii', 'ignore')
+    datafile = StringIO(data)
+    dict_reader = csv.DictReader(datafile)
+    line_count = 0
+
+    for row in dict_reader:
+      if line_count == 0:
+        jsonfile.write('[\n')
+
+      if row['Country/Region'] == 'US':
+        json.dump(row, jsonfile)
+
+        # append commas where necessary
+        if line_count < LAST_US_ROW - 1:
+          jsonfile.write(',')
+          
+        jsonfile.write('\n')
+
+      if line_count == NUM_ROWS - 1:
+        jsonfile.write(']\n')
+
+      line_count += 1
+
+# generates recovered.json
+#
+# reads data from CSV file located at the URL CONFIRMED_CSV_INPUT
+# and outputs the number of confirmed cases by date for all US states
+# as a list of JSON objects, stored in the file at CONFIRMED_JSON_OUTPUT
+with closing(urlopen(RECOVERED_CSV_INPUT)) as csvfile:
+  with open(RECOVERED_JSON_OUTPUT, 'w') as jsonfile:
+    data = csvfile.read().decode('ascii', 'ignore')
+    datafile = StringIO(data)
+    dict_reader = csv.DictReader(datafile)
+    line_count = 0
+
+    for row in dict_reader:
+      if line_count == 0:
+        jsonfile.write('[\n')
+
+      if row['Country/Region'] == 'US':
+        json.dump(row, jsonfile)
+
+        # append commas where necessary
+        if line_count < LAST_US_ROW - 1:
+          jsonfile.write(',')
+          
+        jsonfile.write('\n')
+
+      if line_count == NUM_ROWS - 1:
+        jsonfile.write(']\n')
+
+      line_count += 1
